d1ae2bc6329fdbdd3106a28a80ce39cbb24ae372
Davy <dj.raitt@student.avans.nl>
2020-06-10 16:35:34 +0200

    Initial commit

---
+++ app/src/main/java/com/avans/mediawikiactionapi_davy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.avans.mediawikiactionapi_davy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
